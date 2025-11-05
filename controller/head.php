e849f269637ddcd7fc7916f8b4b1197ffd5f050a
softwarejhond <softwarejhond@gmail.com>
2025-11-05 16:51:31 -0500

    FIX

--- controller/head.php
+++ controller/head.php
@@ -7,12 +7,14 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/floating-button.css">
 
-    <title><?php echo ucfirst(str_replace('.php', '', basename($_SERVER['PHP_SELF']))); ?> | CENDI</title>
+    <title><?php echo ucfirst(str_replace('.php', '', basename($_SERVER['PHP_SELF']))); ?> | CORPORACIÓN CENDI</title>
     	<!-- Start of HubSpot Embed Code  chat-->
 	<script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/21270719.js"></script>
 	<!-- End of HubSpot Embed Code chat -->
